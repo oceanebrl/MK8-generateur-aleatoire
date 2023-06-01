a6b5d18b7da63cfdbb6bb4e98fed78ca061629af
Océane <oceanebrl.94@gmail.com>
2023-06-01 15:18:09 +0200

    footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,7 +1,25 @@
+import styles from "../styles/components/footer.module.scss";
+
 function Footer() {
   return (
-    <footer>
-      <h2>Mon super footer</h2>
+    <footer className={styles.footer}>
+      <div className={styles.wrap}>
+        <p>
+          <span className={styles.highlight}>Disclaimer</span>&#8239;: le site
+          est toujours en construction&#8239;!
+        </p>
+        <p>
+          Pour toutes informations ou suggestions, contactez-moi sur twitter, à{" "}
+          <a href="https://twitter.com/Bocnee" className={styles.link}>
+            @Bocnee
+          </a>
+          , ou par mail à{" "}
+          <a href="mailto:bocnee.aurel@gmail.com" className={styles.link}>
+            bocnee.aurel@gmail.com
+          </a>
+          .
+        </p>
+      </div>
     </footer>
   );
 }
